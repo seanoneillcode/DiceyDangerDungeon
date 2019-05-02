d7ee142816950dbcc81901a3dcb4baef23961b7f
Sean O'Neill <seansaysmeh@gmail.com>
2019-05-02 18:39:02 +0100

    made the perfomr action some delayed steps

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -13,6 +13,7 @@ public class Game : MonoBehaviour
 
     private Actor activeActor;
     private ExplosionHandler explosionHandler;
+    private Node actionedNode;
 
     // Start is called before the first frame update
     void Start()
@@ -27,32 +28,62 @@ public class Game : MonoBehaviour
         {
             if (activeActor.IsAtTarget())
             {
-                Debug.Log("perform the action at the node");
                 Node node = GetOverlap(activeActor);
-                if (node != null && node.risk > 0)
+                if (node != null && node.risk > 0 && node != actionedNode)
                 {
+                    Debug.Log("perform the action at the node");
                     Debug.Log("node is " + node.gameObject.name);
-                    diceResult = UnityEngine.Random.Range(1, 6);
-                    Debug.Log("rolled a " + diceResult);
-                    Debug.Log("risk was " + node.risk);
-                    if (diceResult < node.risk)
-                    {
-                        Debug.Log("hurt the player ");
-                        playerHealth = playerHealth - 1;
-                    }
-                    node.RemoveRisk();
-                    if (node.actor != null)
-                    {
-                        explosionHandler.Explode(node.actor.gameObject.transform.position);
-                        Destroy(node.actor.gameObject);
-                    } else
-                    {
-                        Debug.Log("Node doesn't have an actor");
-                    }
+
+                    actionedNode = node;
+
+                    StartCoroutine(ExecuteAfterTime(0.5f, () => {
+                        RollDiceStep(node);
+                    }));
+
+                    StartCoroutine(ExecuteAfterTime(1.5f, () => {
+                        RemoveActorStep(node);
+                    }));
+
+                    StartCoroutine(ExecuteAfterTime(2.5f, () => {
+                        MoveActorAction(activeActor, node);
+                        actionedNode = null;
+                        diceResult = -1;
+                    }));
                 }
-                activeActor = null;
             }
         }
+    }
+
+    private void RollDiceStep(Node node)
+    {
+        diceResult = UnityEngine.Random.Range(1, 6);
+        Debug.Log("rolled a " + diceResult);
+        Debug.Log("risk was " + node.risk);
+
+        if (diceResult < node.risk)
+        {
+            Debug.Log("hurt the player ");
+            playerHealth = playerHealth - 1;
+        }
+        
+    }
+
+    private void RemoveActorStep(Node node)
+    {
+        if (node.actor != null)
+        {
+            explosionHandler.Explode(node.actor.gameObject.transform.position);
+            Destroy(node.actor.gameObject);
+        }
+        else
+        {
+            Debug.Log("Node doesn't have an actor");
+        }
+        node.RemoveRisk();
+    }
+
+    private void MoveActorAction(Actor actor, Node node) {
+        actor.targetPos = node.transform.position;
     }
 
     private Node GetOverlap(Actor actor)
@@ -81,10 +112,21 @@ public class Game : MonoBehaviour
         if (selectedActor != null && node != null)
         {
             activeActor = selectedActor;
-            Vector3 direction = Vector3.Normalize(node.transform.position - selectedActor.transform.position);
-            selectedActor.targetPos = node.transform.position - (direction * 0.8f);
-            selectedActor = null;
+            if (node.actor != null)
+            {
+                Vector3 direction = Vector3.Normalize(node.transform.position - selectedActor.transform.position);
+                selectedActor.targetPos = node.transform.position - (direction * 0.8f);
+            } else
+            {
+                selectedActor.targetPos = node.transform.position;
+            }
         }
         selectedNode = node;
+    }
+
+    IEnumerator ExecuteAfterTime(float time, Action task)
+    {
+        yield return new WaitForSeconds(time);
+        task();
     }
 }
